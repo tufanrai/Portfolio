2de3283110207dcb1e99ed736e3c5f7a0ba1e215
TufanRai <thulungtufan16@gmail.com>
2025-07-09 09:15:54 +0545

    updated the dark and light feature

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -1,42 +1,77 @@
+"use client";
 import Particles from "@/animation/Particles/Particles";
 import profile from "@/src/public/profile.jpg";
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
 import { FaGithub } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { AbtMe } from "@/src/utils/contents";
+import { DayContext } from "@/src/app/(dashboard)/layout";
 
-const AboutMe = () => {
+function AboutMe() {
+  const Day = useContext(DayContext);
   return (
     <div className="w-full h-screen flex items-start justify-center">
       <div className=" w-full h-full fixed sm:scale-110 sm:left-25">
-        <Particles
-          moveParticlesOnHover
-          alphaParticles
-          className="w-full h-screen"
-        />
+        {Day && Day ? (
+          <Particles
+            moveParticlesOnHover
+            alphaParticles
+            className={`w-full h-screen `}
+            particleColors={["#000000"]}
+          />
+        ) : (
+          <Particles
+            moveParticlesOnHover
+            alphaParticles
+            className={`w-full h-screen `}
+            particleColors={["#ffffff"]}
+          />
+        )}
       </div>
-      <div className="sm:mt-20 rounded-md text-white backdrop-blur-sm px-8 py-4 font-thin flex flex-col-reverse sm:flex-row items-start justify-center sm:flex-row sm:items-center sm:justify-center sm:gap-2 gap-4">
+      <div
+        className={`sm:mt-20 rounded-md backdrop-blur-sm ${
+          Day && Day ? "text-black" : "text-white"
+        } px-8 py-4 font-thin flex flex-col-reverse sm:flex-row items-start justify-center sm:flex-row sm:items-center sm:justify-center sm:gap-2 gap-4`}
+      >
         <div>
           <h2 className="font-semibold text-lg">About me!</h2>
-          <p className="font-thin text-sm text-neutral-300">{AbtMe.head}</p>
+          <p className="font-thin text-sm">{AbtMe.head}</p>
           <br />
-          <p className="font-thin text-sm text-neutral-300">{AbtMe.body}</p>
+          <p className="font-thin text-sm">{AbtMe.body}</p>
           <div className="w-full mt-4">
             <ul className="flex items-center justify-start gap-1">
               <Link href={AbtMe.media1}>
-                <li className="font-thin text-lg text-neutral-200 px-2 py-1 cursor-pointer ease duration-200 hover:text-neutral-100 hover:scale-110">
+                <li
+                  className={`font-thin text-lg px-2 py-1 cursor-pointer ease duration-200 ${
+                    Day && Day
+                      ? "text-black/25 hover:text-black"
+                      : "text-white/25 hover:text-white"
+                  } hover:scale-110`}
+                >
                   <FaGithub />
                 </li>
               </Link>
               <Link href={AbtMe.media2}>
-                <li className="font-thin text-lg text-neutral-200 px-2 py-1 cursor-pointer ease duration-200 hover:text-neutral-100 hover:scale-110">
+                <li
+                  className={`font-thin text-lg px-2 py-1 cursor-pointer ease duration-200 ${
+                    Day && Day
+                      ? "text-black/25 hover:text-black"
+                      : "text-white/25 hover:text-white"
+                  } hover:scale-110`}
+                >
                   <FaInstagram />
                 </li>
               </Link>
               <Link href={AbtMe.media3}>
-                <li className="font-thin text-lg text-neutral-200 px-2 py-1 cursor-pointer ease duration-200 hover:text-neutral-100 hover:scale-110">
+                <li
+                  className={`font-thin text-lg px-2 py-1 cursor-pointer ease duration-200 ${
+                    Day && Day
+                      ? "text-black/25 hover:text-black"
+                      : "text-white/25 hover:text-white"
+                  } hover:scale-110`}
+                >
                   <FaLinkedin />
                 </li>
               </Link>
@@ -52,6 +87,6 @@ const AboutMe = () => {
       </div>
     </div>
   );
-};
+}
 
 export default AboutMe;
