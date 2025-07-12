d70ed17d01e0969bc62d9dc5acb5b30bbab67016
TufanRai <thulungtufan16@gmail.com>
2025-07-12 21:28:13 +0545

    worked on work/project page

--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Work } from "@/src/utils/contents";
+import Link from "next/link";
 
 const Works = () => {
   return (
@@ -6,11 +8,31 @@ const Works = () => {
       <div className="px-8 py-4">
         <h1 className="font-bold text-lg">Work/Project</h1>
       </div>
-      <div className="px-8 py-4">
-        <h1 className="font-thin text-xl italic">Coming Soon!</h1>
-        <h3 className="font-thin text-sm">
-          This page is in under construction...
-        </h3>
+      <div className="px-8 py-4 w-full h-screen flex items-start justify-start gap-2 overflow-hidden flex-wrap">
+        {Work.map((work, index) => (
+          <div
+            className="sm:w-54 w-45 rounded-lg border overflow-hidden"
+            key={index}
+          >
+            <div className="w-full h-[50%] bg-white overflow-hidden cursor-pointer hover:scale-110 ease duration-300">
+              <img
+                src={`${work.img}`}
+                className="object-cover w-full object-center"
+                alt=""
+              />
+            </div>
+            <div className="py-4 px-2 flex flex-col items-start justify-center gap-1">
+              <h1 className="text-lg">{work.title}</h1>
+              <p className="text-sm text-neutral-400">{work.description}</p>
+              <Link
+                href={work.link}
+                className="font-light text-sm text-neutral-500 underline ease duration-300 hover:text-neutral-300"
+              >
+                {work.link}
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
